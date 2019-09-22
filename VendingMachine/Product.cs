3de8c765d76748820d874916d305d2d77b31c363
Kacper <Kacper@DESKTOP-CDNO7J9>
2019-09-22 13:29:50 +0200

    Program Design

---
+++ VendingMachine/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    class Product
+    {
+        public string ProdName { get; set; }
+        public decimal ProdCost { get; set; }
+
+    }
+}
